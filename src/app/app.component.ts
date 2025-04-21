a437346ff1179c42ce4f48bf662d7d5c1f1a132e
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-04-21 16:22:10 -0300

    Name filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/users-list';
+import { IFilterOptions } from './interfaces/filter-options.interface';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,9 @@ import { UsersList } from './data/users-list';
 export class AppComponent implements OnInit {
 
   title = 'Angular-UserFilter';
-
   selectedUser: IUser | undefined;
-
   usersList: IUser[] = [];
+  usersListFiltered: IUser[] = [];
 
   changeSelectedUser(user: IUser) {
    this.selectedUser = user; 
@@ -23,6 +23,25 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     setTimeout(() => {
       this.usersList = UsersList
+      this.usersListFiltered = this.usersList;
     }, 1500);
+  }
+
+  onFilter(filterOptions: IFilterOptions) {
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
+  }
+
+  filterUsersList(filterOptions: IFilterOptions, usersList: IUser[]): IUser[] {
+    let filteredList: IUser[] = [];
+    filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+
+    return filteredList;
+  }
+  filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
+    const NAME_NOT_TYPPED = name === undefined;
+    if (NAME_NOT_TYPPED) return usersList;
+
+    const filteredList = usersList.filter(u => u.name.toLowerCase().includes(name.toLowerCase()));
+    return filteredList;
   }
 }
